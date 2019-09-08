addfa924d37ac4dd982e175956332a493a684975
Wellington Duarte <wellington400@msn.com>
2019-09-08 00:38:56 -0300

    Adicionada nova versão do projeto

---
+++ 2019-2_DevWeb07/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2019_2_DevWeb07.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
